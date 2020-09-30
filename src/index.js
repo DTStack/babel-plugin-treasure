81f558b6db46b2d1847b14f97888af941f65c828
chenfeng <chenfeng@dtstack.com>
2020-09-30 11:25:12 +0800

    feat: compatible with Babel < 7 version

--- src/index.js
+++ src/index.js
@@ -30,26 +30,64 @@ export default function({ types }) {
    */
   const Program = {
     enter(path, { opts = {} }) {
-      assert(opts.libraryName, 'libraryName should be provided');
-      plugins = [
-        new Plugin(
-          opts.libraryName,
-          opts.libraryDirectory,
-          opts.style,
-          opts.styleLibraryDirectory,
-          opts.customStyleName,
-          opts.camel2DashComponentName,
-          opts.camel2UnderlineComponentName,
-          opts.fileName,
-          opts.customName,
-          opts.transformToDefaultImport,
-          types,
-        ),
-      ];
-      // eslint-disable-next-line prefer-rest-params
-      applyInstance('ProgramEnter', arguments, this);
+      if (!plugins) {
+        if (Array.isArray(opts)) {
+          plugins = opts.map(
+            (
+              {
+                libraryName,
+                libraryDirectory,
+                style,
+                styleLibraryDirectory,
+                customStyleName,
+                camel2DashComponentName,
+                camel2UnderlineComponentName,
+                fileName,
+                customName,
+                transformToDefaultImport,
+              },
+              index,
+            ) => {
+              assert(libraryName, 'libraryName should be provided');
+              return new Plugin(
+                libraryName,
+                libraryDirectory,
+                style,
+                styleLibraryDirectory,
+                customStyleName,
+                camel2DashComponentName,
+                camel2UnderlineComponentName,
+                fileName,
+                customName,
+                transformToDefaultImport,
+                types,
+                index,
+              );
+            },
+          );
+        } else {
+          assert(opts.libraryName, 'libraryName should be provided');
+          plugins = [
+            new Plugin(
+              opts.libraryName,
+              opts.libraryDirectory,
+              opts.style,
+              opts.styleLibraryDirectory,
+              opts.customStyleName,
+              opts.camel2DashComponentName,
+              opts.camel2UnderlineComponentName,
+              opts.fileName,
+              opts.customName,
+              opts.transformToDefaultImport,
+              types,
+            ),
+          ];
+        }
+      }
+      applyInstance('ProgramEnter', arguments, this); // eslint-disable-line
     },
     exit() {
+      // eslint-disable-next-line prefer-rest-params
       applyInstance('ProgramExit', arguments, this);
     },
   };
@@ -59,6 +97,7 @@ export default function({ types }) {
     'CallExpression',
     'MemberExpression',
     'ClassDeclaration',
+    'Property',
     'ConditionalExpression',
     'ReturnStatement',
     'IfStatement',
